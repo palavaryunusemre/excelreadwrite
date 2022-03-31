f4365cf9b1ebe2db95c7457d2bdaf3cf8b488d95
palavaryunusemre <yunusemrepalavar.98@gmail.com>
2022-03-31 05:46:19 +0300

    second commit

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import pandas as pd
 
-path=".xls"
+path="TDMS.xls"
 sheet_name1="TDMS"
 sheet_name2="Sayfa1"
+sheet_name3="Sayfa2"
 data=pd.read_excel(path,sheet_name=sheet_name1)
 df=pd.DataFrame(data)
 
-fisDate=df['fistarihi']
-fisNo=df['fisno']
-tfiNo=df['tasinirislemno']
-dsc=df['dsc']
-borc=df['borc']
-alacak=df['alacak']
+data1=pd.read_excel(path,sheet_name=sheet_name2)
+df2=pd.DataFrame(data1)
+
+data2=pd.read_excel(path, sheet_name=sheet_name3)
+df3=pd.DataFrame(data2)
+
+
+#---------------------------data
+fisDate=df['FİŞ TARİHİ']
+fisNo=df['FİŞ NO']
+tfiNo=df['TAŞINIR İŞLEM FİŞ NO']
+dsc=df['DSC']
+borc=df['BORÇ']
+alacak=df['ALACAK']
+#-----------------------data1
+data1Tifno=df2['TİF No']
+#------------------------data2
+data2tifno=df3['Tif NO']
+#--------------------------
+dizi=[]
+"""for i in range(len(data2tifno)):
+    if (data1Tifno[i] == data2tifno[i]):
+       dizi.append('true')
+       i=i+1
+    else:
+        dizi.append('false')
+        i=i+1"""
+sayac=0
+"""for i in data2tifno:
+    if (data2tifno[i].values == data1Tifno[sayac].values):
+        dizi.append('true')
+    else:
+        dizi.append('false')
+"""
+
+
 #----------------------------------------------------
 fisNo=fisNo.fillna("")
 fisNo=fisNo.to_numpy()
@@ -28,10 +59,10 @@ resultDate=pd.DataFrame()
 resultDsc=pd.DataFrame()
 resultBorc=pd.DataFrame()
 resultAlacak=pd.DataFrame()
+resultTfiCheck=pd.DataFrame()
 #-----------------------------------------------
 
-
-x=tfiNo.str.split('-')
+x=tfiNo.str.split(r"-|/")
 
 
 arrTfi=[]
@@ -41,6 +72,7 @@ arrDsc=[]
 arrBorc=[]
 arrAlacak=[]
 
+
 index=0
 for i in x:
     for j in i:
@@ -48,11 +80,9 @@ for i in x:
         arrNo.append(fisNo[index])
         arrDate.append(fisDate[index])
         arrDsc.append(dsc[index])
-        arrBorc.append(borc[index])
+        arrBorc.append(borc[index]/len(i))
         arrAlacak.append(alacak[index])
-    index+=1
-
-
+    index += 1
 
 
 resultTfi['Tif NO'] = pd.DataFrame(arrTfi)
